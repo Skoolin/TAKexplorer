103c0d1486fe489e02937c1a3fa97c18b060ce18
nitzel <jan.s.92@web.de>
2023-06-06 16:01:33 +0200

    fix: database_release gitlab job

--- util/dump_db.py
+++ util/dump_db.py
@@ -1,57 +1,40 @@
+from contextlib import closing
 import csv
 import sqlite3
 
-con = sqlite3.connect('data/openings_s6_1200.db')
-con.row_factory = sqlite3.Row
+with closing(sqlite3.connect('data/openings_s6_1200.db')) as con:
+    con.row_factory = sqlite3.Row
 
-cur = con.cursor()
+    with closing(con.cursor()) as cur:
+        # dump positions
+        cur.execute("SELECT * FROM positions;")
+        with open("positions.csv", "w", encoding="UTF-8") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter="\t")
+            csv_writer.writerow([i[0] for i in cur.description])
+            csv_writer.writerows(cur)
 
-# dump positions
-cur.execute("SELECT * FROM positions;")
-with open("positions.csv", "w") as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter="\t")
-    csv_writer.writerow([i[0] for i in cur.description])
-    csv_writer.writerows(cur)
+        # dump games
+        cur.execute("SELECT * FROM games;")
+        with open("games.csv", "w", encoding="UTF-8") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter="\t")
+            csv_writer.writerow([i[0] for i in cur.description])
+            csv_writer.writerows(cur)
 
-# dump moves
-cur.execute("""
-SELECT moves_map.*, positions.bwins, positions.wwins
-FROM moves_map, positions
-WHERE positions.id = moves_map.next_position_id
-ORDER BY position_id;
-""")
-with open("moves.csv", "w") as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter="\t")
-    csv_writer.writerow([i[0] for i in cur.description])
-    csv_writer.writerows(cur)
+        # dump reference table for games and positions
+        cur.execute("SELECT * FROM game_position_xref ORDER BY position_id;")
+        with open("game_references.csv", "w", encoding="UTF-8") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter="\t")
+            csv_writer.writerow([i[0] for i in cur.description])
+            csv_writer.writerows(cur)
 
-# dump games
-cur.execute("SELECT * FROM games;")
-with open("games.csv", "w") as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter="\t")
-    csv_writer.writerow([i[0] for i in cur.description])
-    csv_writer.writerows(cur)
+total: str = ""
 
-# dump reference table for games and positions
-cur.execute("SELECT * FROM game_position_xref ORDER BY position_id;")
-with open("game_references.csv", "w") as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter="\t")
-    csv_writer.writerow([i[0] for i in cur.description])
-    csv_writer.writerows(cur)
-
-# done!
-con.close()
-
-total = ""
-
-with open("positions.csv") as t:
+with open("positions.csv", encoding="UTF-8") as t:
     total += t.read() + "\nTABLE\n"
-with open("games.csv") as t:
+with open("games.csv", encoding="UTF-8") as t:
     total += t.read() + "\nTABLE\n"
-with open("moves.csv") as t:
-    total += t.read() + "\nTABLE\n"
-with open("game_references.csv") as t:
+with open("game_references.csv", encoding="UTF-8") as t:
     total += t.read()
 
-with open("db_dump", "w") as t:
+with open("db_dump", "w", encoding="UTF-8") as t:
     t.write(total)
